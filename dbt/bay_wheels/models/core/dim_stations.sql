80d1c4bf31439c2e145e724b87bdfaa494a8b335
Ed ed <emuzo@uoc.edu>
2025-04-17 01:01:20 +0000

    Correct dim stations

--- dbt/bay_wheels/models/core/dim_stations.sql
+++ dbt/bay_wheels/models/core/dim_stations.sql
@@ -22,14 +22,24 @@ WITH station_data AS (
     end_lng AS lng
   FROM {{ ref('stg_baywheels_trips') }}
   WHERE end_station_name IS NOT NULL AND end_station_id IS NOT NULL
-), clean_station_data AS (
-SELECT
-  station_id,
-  ANY_VALUE(station_name) AS station_name,
-  ANY_VALUE(lat) AS lat,
-  ANY_VALUE(lng) AS lng
-FROM station_data
-GROUP BY station_id
+)
+, station_area_data AS (
+  SELECT
+    s.*,
+    a.county_id, a.county_name
+  FROM station_data s
+  LEFT JOIN `de-zoomcamp-bay-wheels.bike_data_all.dim_bay_area_county` a
+  ON ST_WITHIN(ST_GEOGPOINT(lng, lat), a.geometry)   
+  WHERE a.county_id IS NOT NULL
+)
+, clean_station_data AS (
+  SELECT
+    station_id,
+    ANY_VALUE(station_name) AS station_name,
+    ANY_VALUE(lat) AS lat,
+    ANY_VALUE(lng) AS lng
+  FROM station_area_data
+  GROUP BY station_id
 )
 SELECT 
   *,
